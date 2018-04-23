865d796a194760a2069b9c1d8838ec0ef9529d4f
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-04-23 23:38:52 +0200

    Step 3 done

--- src/my-monolithic-app.js
+++ src/my-monolithic-app.js
@@ -1,8 +1,16 @@
 (function() {
     "use strict";
 
+    function Hero(data) {
+        const self = this;
+        
+        self.id = data.id;
+        self.name = ko.observable(data.name);
+    }
+    
     const appModel = {
         title: "Tour of Heroes",
+        hero: new Hero({ id: 1, name: "Windstorm" }),
     };
     
     document.addEventListener("DOMContentLoaded", () => {
